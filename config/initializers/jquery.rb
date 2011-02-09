1bcdbab6890b359965dc527df3f390fc533ce535
Jonathan Linowes <linojon@gmail.com>
2011-02-09 01:19:27 -0500

    first draft

--- config/initializers/jquery.rb
+++ config/initializers/jquery.rb
@@ -4,9 +4,9 @@
 	# Written by: Logan Leger, logan@loganleger.com
 	# http://github.com/lleger/Rails-3-jQuery
 
-	ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery', 'rails']
+	ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.4.4.min', 'rails']
 	ActiveSupport.on_load(:action_view) do
 	  ActiveSupport.on_load(:after_initialize) do
-	    ActionView::Helpers::AssetTagHelper::register_javascript_expansion :defaults => ['jquery', 'rails']
+	    ActionView::Helpers::AssetTagHelper::register_javascript_expansion :defaults => ['jquery-1.4.4.min', 'rails']
 	  end
 	end
