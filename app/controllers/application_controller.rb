ee84bfae550947b82678429f94c280f9b17126dc
Jonathan Linowes <linojon@gmail.com>
2011-03-18 13:37:03 -0400

    redirect 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,4 @@ class ApplicationController < ActionController::Base
   include Authentication
   protect_from_forgery
   
-  # Example controller helper:
-  
-  # helper_method :my_buildings
-  # 
-  # def my_buildings
-  #   if admin?
-  #     Building.all 
-  #   else
-  #     current_user.buildings
-  #   end
-  # end
-  
 end
